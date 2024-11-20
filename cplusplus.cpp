73dd8949a6ffbda26affddb304d2d4baf253b61c
BACSIHIEN1 <150429508+BACSIHIEN1@users.noreply.github.com>
2024-11-20 23:54:41 +0700

    add new file

---
+++ cplusplus.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define FOR(i,a,b) for(int i = a;i < b;i++)
+#define FORD(i,a,b) for(int i = a;i >= b;i--)
+#define pb push_back
+#define fi first
+#define se second
+#define faster() ios_base::sync_with_stdio(false); cin.tie(NULL);
+using ll = long long;
+using namespace std;
+int main(){
+    cout << "HELLO world";
+    return 0;
+}
